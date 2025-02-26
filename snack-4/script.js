712bad84f9d42214c76b9349cde7846e4b0a97cc
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-02-26 16:52:50 +0100

    first commit

---
+++ snack-4/script.js
@@ -0,0 +1,8 @@
+// # SNACK 4
+/* 
+Snack 4 - Calcola l’età media
+
+    Creare un array (ages) che contiene le età degli autori dei libri.
+    Calcola la somma delle età (agesSum) usando reduce.
+    Stampa in console l’età media degli autori dei libri.
+*/
